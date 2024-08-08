58856060d77dd8a9256621f84952c153fccab190
Ashish Prasad <ashish.ap.rao@gmail.com>
2024-08-08 09:51:23 +0530

    Added logging

--- src/game.c
+++ src/game.c
@@ -1,6 +1,7 @@
 #include "include/game.h"
 #include "include/fen.h"
 #include "include/position.h"
+#include "lib/logc/log.h"
 #include <stdlib.h>
 
 Game* initialize_game(const char* fen) {
@@ -8,8 +9,13 @@ Game* initialize_game(const char* fen) {
     if (game == NULL)
     {
         // TODO: Raise exception - Memory Error!
+
+        log_fatal("Memory allocation failed!");
         exit(1);
     }
+
+    log_trace("Initializing game with FEN: %s", fen);
     game->position = fen_to_position(fen);
+
     return game;
 }
