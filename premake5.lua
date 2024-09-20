48e5bac3d43d3562dd5b4f9865498cc89e85dd0d
Ashish Prasad <ashish.ap.rao@gmail.com>
2024-09-20 18:16:47 +0530

    Added platforms and flags to premake

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 solution "pixie"
     configurations { "Debug", "Release" }
+    platforms { "MacOS64", "Win64" }
 
 project "pixie"
     kind "ConsoleApp"
@@ -11,10 +12,20 @@ project "pixie"
     files { "src/**.h", "src/**.c" }
     removefiles { "src/pypixie.c" }
 
+    filter { "platforms:MacOS64" }
+        system "macosx"
+        architecture "x86_64"
+
+    filter { "platforms:Win64" }
+        system "Windows"
+        architecture "x86_64"
+
     filter "configurations:Debug"
+        flags { "FatalWarnings" }
         defines { "DEBUG" }
         symbols "On"
 
     filter "configurations:Release"
+        flags { "FatalWarnings", "LinkTimeOptimization" }
         defines { "NDEBUG" }
         optimize "On"
