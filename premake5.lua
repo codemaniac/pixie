48901726568f388dc2cacbaf5002edd8b2ec9360
Ashish Prasad <ashish.ap.rao@gmail.com>
2024-09-26 00:59:11 +0530

    Modified build script

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,17 @@
 workspace "pixie"
     configurations { "Debug", "Release" }
+    version = "0.1.0-beta"
+
+    function getTargetName()
+        local os_name = os.target()
+        local arch_name = "x86_64"
+        return "%{prj.name}_" .. os_name .. "_" .. arch_name .. "_v" .. version
+    end
 
 project "pixie"
     kind "ConsoleApp"
     language "C"
-    targetname ("pixie")
+    targetname (getTargetName())
     targetdir "bin/%{cfg.buildcfg}"
     entrypoint ("main()")
     includedirs { "src/include" }
