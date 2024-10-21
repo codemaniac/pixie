e89bf708e7124abd0c6d43fb1baa42f6dfc49be9
Ashish Prasad <ashish.ap.rao@gmail.com>
2024-10-21 11:55:02 +0530

    📦 NEW: UCI stop command

--- src/search.cpp
+++ src/search.cpp
@@ -8,10 +8,85 @@
 #include <cstdint>
 #include <iostream>
 #include <memory>
+#include <unistd.h>
+#ifdef WIN32
+    #include "windows.h"
+#endif
+
+// http://home.arcor.de/dreamlike/chess/
+int InputWaiting() {
+#ifndef WIN32
+    fd_set         readfds;
+    struct timeval tv;
+    FD_ZERO(&readfds);
+    FD_SET(fileno(stdin), &readfds);
+    tv.tv_sec  = 0;
+    tv.tv_usec = 0;
+    select(16, &readfds, 0, 0, &tv);
+
+    return (FD_ISSET(fileno(stdin), &readfds));
+#else
+    static int    init = 0, pipe;
+    static HANDLE inh;
+    DWORD         dw;
+
+    if (!init)
+    {
+        init = 1;
+        inh  = GetStdHandle(STD_INPUT_HANDLE);
+        pipe = !GetConsoleMode(inh, &dw);
+        if (!pipe)
+        {
+            SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT | ENABLE_WINDOW_INPUT));
+            FlushConsoleInputBuffer(inh);
+        }
+    }
+    if (pipe)
+    {
+        if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL))
+            return 1;
+        return dw;
+    }
+    else
+    {
+        GetNumberOfConsoleInputEvents(inh, &dw);
+        return dw <= 1 ? 0 : dw;
+    }
+#endif
+}
+
+void ReadInput(SearchInfo* info) {
+    int  bytes;
+    char input[256] = "", *endc;
+
+    if (InputWaiting())
+    {
+        info->stopped = true;
+        do
+        {
+            bytes = read(fileno(stdin), input, 256);
+        } while (bytes < 0);
+        endc = strchr(input, '\n');
+        if (endc)
+            *endc = 0;
+
+        if (strlen(input) > 0)
+        {
+            if (!strncmp(input, "quit", 4))
+            {
+                info->stopped = true;
+                exit(EXIT_SUCCESS);
+            }
+        }
+        return;
+    }
+}
 
 static void _search_check_up(SearchInfo* info) {
     if (info->timeset && utils_get_current_time_in_milliseconds() >= info->stoptime)
         info->stopped = true;
+
+    ReadInput(info);
 }
 
 static int32_t _search_quiescence(std::unique_ptr<Position>& position,
