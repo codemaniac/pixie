3d95f75b5e1b2c18787e770ee9786eb677044fcc
Ashish Prasad <ashish.ap.rao@gmail.com>
2024-11-09 13:57:41 +0530

    🐛 FIX: Passed Pawn logic

--- src/eval.cpp
+++ src/eval.cpp
@@ -208,13 +208,13 @@ int32_t eval_position(std::unique_ptr<Position>& position) {
             case WHITE_PAWN :
                 eval += POSITIONAL_SCORE_PAWN[SQUARES_MIRRORED[sq]];
 
-                doubled_pawns = utils_bit_count1s(wP_bb & MASK_SQ_FILE[sq]) - 1;
+                doubled_pawns = utils_bit_count1s(wP_bb & MASK_SQ_FILE[sq]);
                 eval += (DOUBLE_PAWN_PENALTY * doubled_pawns);
 
                 is_isolated_pawn = utils_bit_count1s(wP_bb & MASK_PAWN_ISOLATED[sq]) == 0;
                 eval += (ISOLATED_PAWN_PENALTY * is_isolated_pawn);
 
-                is_passed_pawn = utils_bit_count1s(bP_bb && MASK_PAWN_PASSED_WHITE[sq]) == 0;
+                is_passed_pawn = utils_bit_count1s(bP_bb & MASK_PAWN_PASSED_WHITE[sq]) == 0;
                 eval += PASSED_PAWN_BONUS[BOARD_SQ_TO_RANK(static_cast<Square>(sq))];
 
                 break;
@@ -237,13 +237,13 @@ int32_t eval_position(std::unique_ptr<Position>& position) {
             case BLACK_PAWN :
                 eval -= POSITIONAL_SCORE_PAWN[sq];
 
-                doubled_pawns = utils_bit_count1s(bP_bb & MASK_SQ_FILE[sq]) - 1;
+                doubled_pawns = utils_bit_count1s(bP_bb & MASK_SQ_FILE[sq]);
                 eval -= (DOUBLE_PAWN_PENALTY * doubled_pawns);
 
                 is_isolated_pawn = utils_bit_count1s(bP_bb & MASK_PAWN_ISOLATED[sq]) == 0;
                 eval -= (ISOLATED_PAWN_PENALTY * is_isolated_pawn);
 
-                is_passed_pawn = utils_bit_count1s(wP_bb && MASK_PAWN_PASSED_BLACK[sq]) == 0;
+                is_passed_pawn = utils_bit_count1s(wP_bb & MASK_PAWN_PASSED_BLACK[sq]) == 0;
                 eval -= PASSED_PAWN_BONUS[RANK_8 - BOARD_SQ_TO_RANK(static_cast<Square>(sq))];
 
                 break;
