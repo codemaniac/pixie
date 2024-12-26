0811c35885a0b9969da978a5dc8f831244d3dbf2
Ashish Prasad <ashish.ap.rao@gmail.com>
2024-12-26 20:00:32 +0530

    👌 IMPROVE: Gradient update

--- src/tune.cpp
+++ src/tune.cpp
@@ -23,8 +23,8 @@ enum Phase {
 };
 
 struct Entry {
-    ArrayList<int32_t, N_PARAMS> coefficients[2];  // 0 = WHITE, 1 = BLACK
-    double                       wdl, phase, pfactors[2];
+    ArrayList<double, N_PARAMS> coefficients;
+    double                      wdl, phase;
 };
 
 constexpr int32_t S(const int32_t mg, const int32_t eg) {
@@ -89,28 +89,23 @@ static double get_phase(const Position& position) {
 static void init_entry_coefficients(Entry* entry, const Position& position) {
     int param_index = 0;
 
-    entry->coefficients[WHITE][param_index]   = position.get_piece_count(WHITE_PAWN);
-    entry->coefficients[BLACK][param_index++] = position.get_piece_count(BLACK_PAWN);
-
-    entry->coefficients[WHITE][param_index]   = position.get_piece_count(WHITE_KNIGHT);
-    entry->coefficients[BLACK][param_index++] = position.get_piece_count(BLACK_KNIGHT);
-
-    entry->coefficients[WHITE][param_index]   = position.get_piece_count(WHITE_BISHOP);
-    entry->coefficients[BLACK][param_index++] = position.get_piece_count(BLACK_BISHOP);
-
-    entry->coefficients[WHITE][param_index]   = position.get_piece_count(WHITE_ROOK);
-    entry->coefficients[BLACK][param_index++] = position.get_piece_count(BLACK_ROOK);
-
-    entry->coefficients[WHITE][param_index]   = position.get_piece_count(WHITE_QUEEN);
-    entry->coefficients[BLACK][param_index++] = position.get_piece_count(BLACK_QUEEN);
-
-    entry->coefficients[WHITE][param_index]   = position.get_piece_count(WHITE_KING);
-    entry->coefficients[BLACK][param_index++] = position.get_piece_count(BLACK_KING);
+    entry->coefficients[param_index++] =
+      position.get_piece_count(WHITE_PAWN) - position.get_piece_count(BLACK_PAWN);
+    entry->coefficients[param_index++] =
+      position.get_piece_count(WHITE_KNIGHT) - position.get_piece_count(BLACK_KNIGHT);
+    entry->coefficients[param_index++] =
+      position.get_piece_count(WHITE_BISHOP) - position.get_piece_count(BLACK_BISHOP);
+    entry->coefficients[param_index++] =
+      position.get_piece_count(WHITE_ROOK) - position.get_piece_count(BLACK_ROOK);
+    entry->coefficients[param_index++] =
+      position.get_piece_count(WHITE_QUEEN) - position.get_piece_count(BLACK_QUEEN);
+    entry->coefficients[param_index++] =
+      position.get_piece_count(WHITE_KING) - position.get_piece_count(BLACK_KING);
 }
 
 static void init_parameters(ArrayList<ParameterTuple, N_PARAMS>* parameters) {
 
-    for (auto p : material)
+    for (const auto& p : material)
     {
         double p_mg = mg_score(p);
         double p_eg = eg_score(p);
@@ -128,7 +123,7 @@ static void init_entries(std::vector<Entry>* entries, std::vector<std::string>& 
     for (const auto& fen : data)
     {
         Position position;
-        fen_to_position(fen, &position);
+        fen_to_position(fen, &position, false);
 
         if (position.is_in_check())
             continue;
@@ -137,8 +132,6 @@ static void init_entries(std::vector<Entry>* entries, std::vector<std::string>& 
         entry.wdl          = get_fen_wdl(fen, position.get_active_color());
         const double phase = get_phase(position);
         entry.phase        = phase;
-        entry.pfactors[MG] = 1 - phase / 24.0;
-        entry.pfactors[EG] = 0 + phase / 24.0;
         init_entry_coefficients(&entry, position);
 
         entries->push_back(entry);
@@ -153,15 +146,12 @@ static double linear_eval(const Entry&                               entry,
 
     for (int i = 0; i < N_PARAMS; i++)
     {
-        midgame += (entry.coefficients[WHITE][i] * parameters[i][MG]);
-        midgame -= (entry.coefficients[BLACK][i] * parameters[i][MG]);
-
-        endgame += (entry.coefficients[WHITE][i] * parameters[i][EG]);
-        endgame -= (entry.coefficients[BLACK][i] * parameters[i][EG]);
+        midgame += entry.coefficients[i] * parameters[i][MG];
+        endgame += entry.coefficients[i] * parameters[i][EG];
     }
 
     // Linear interpolation for Tapered Evaluation
-    score = ((midgame * (256 - entry.phase)) + (endgame * entry.phase)) / 256;
+    score = (midgame * entry.phase + endgame * (24 - entry.phase)) / 24;
 
     return score;
 }
@@ -182,8 +172,65 @@ static double get_average_error(const std::vector<Entry>&                  entri
     return avg_error;
 }
 
+static void update_single_gradient(ArrayList<std::array<double, 2>, N_PARAMS>* gradient,
+                                   const Entry&                                entry,
+                                   const ArrayList<ParameterTuple, N_PARAMS>&  parameters,
+                                   const double                                K) {
+    const double eval = linear_eval(entry, parameters);
+    const double sig  = sigmoid(K, eval);
+    const double res  = (entry.wdl - sig) * sig * (1 - sig);
+
+    const double mg_base = res * (entry.phase / 24.0);
+    const double eg_base = res - mg_base;
+
+    for (int i = 0; i < N_PARAMS; i++)
+    {
+        gradient->at(i)[MG] += mg_base * entry.coefficients.at(i);
+        gradient->at(i)[EG] += eg_base * entry.coefficients.at(i);
+    }
+}
+
+static void compute_gradient(ArrayList<std::array<double, 2>, N_PARAMS>* gradient,
+                             const std::vector<Entry>&                   entries,
+                             const ArrayList<ParameterTuple, N_PARAMS>&  parameters,
+                             const double                                K) {
+    for (const Entry& entry : entries)
+    {
+        update_single_gradient(gradient, entry, parameters, K);
+    }
+}
+
+static double find_optimal_k(const std::vector<Entry>&                  entries,
+                             const ArrayList<ParameterTuple, N_PARAMS>& parameters) {
+
+    const double rate           = 10;
+    const double delta          = 1e-5;
+    const double deviation_goal = 1e-6;
+    double       K              = 2.5;
+    double       deviation      = 1;
+
+    while (fabs(deviation) > deviation_goal)
+    {
+        const double up   = get_average_error(entries, parameters, K + delta);
+        const double down = get_average_error(entries, parameters, K - delta);
+        deviation         = (up - down) / (2 * delta);
+        std::cout << "Current K: " << K << ", up: " << up << ", down: " << down
+                  << ", deviation: " << deviation << std::endl;
+        K -= deviation * rate;
+    }
+
+    return K;
+}
+
+static void print_parameters(const ArrayList<ParameterTuple, N_PARAMS>& parameters) {
+    for (const ParameterTuple& p : parameters)
+    {
+        std::cout << p[MG] << "\t" << p[EG] << std::endl;
+    }
+}
+
 void tune() {
-    std::cout << "Tuning" << std::endl;
+    std::cout << "Starting tuning" << std::endl;
 
     std::vector<std::string> data;
     std::filesystem::path    filepath = std::filesystem::current_path() / "data" / "data.epd";
@@ -201,9 +248,49 @@ void tune() {
     init_parameters(&parameters);
     init_entries(&entries, data);
 
-    const double K = 2.5;
+    const double K = find_optimal_k(entries, parameters);
 
     const double avg_error = get_average_error(entries, parameters, K);
 
-    std::cout << "Avg Error = " << avg_error << std::endl;
+    std::cout << "Initial error = " << avg_error << std::endl;
+
+    double  learning_rate  = 0.01;
+    int32_t max_tune_epoch = 1000;
+
+    const double beta1 = 0.9;
+    const double beta2 = 0.999;
+
+    ArrayList<std::array<double, 2>, N_PARAMS> momentum;
+    ArrayList<std::array<double, 2>, N_PARAMS> velocity;
+
+    for (int32_t epoch = 1; epoch < max_tune_epoch; epoch++)
+    {
+        ArrayList<std::array<double, 2>, N_PARAMS> gradient;
+
+        compute_gradient(&gradient, entries, parameters, K);
+
+        for (int parameter_index = 0; parameter_index < N_PARAMS; parameter_index++)
+        {
+            for (int phase_stage = 0; phase_stage < 2; phase_stage++)
+            {
+                const double grad =
+                  -K / 400.0 * gradient[parameter_index][phase_stage] / entries.size();
+                momentum[parameter_index][phase_stage] =
+                  beta1 * momentum[parameter_index][phase_stage] + (1 - beta1) * grad;
+                velocity[parameter_index][phase_stage] =
+                  beta2 * velocity[parameter_index][phase_stage] + (1 - beta2) * pow(grad, 2);
+                parameters[parameter_index][phase_stage] -=
+                  learning_rate * momentum[parameter_index][phase_stage]
+                  / (static_cast<double>(1e-8) + sqrt(velocity[parameter_index][phase_stage]));
+            }
+        }
+
+        if (epoch % 10 == 0)
+        {
+            const double error = get_average_error(entries, parameters, K);
+            std::cout << "Error = " << error << std::endl;
+        }
+    }
+
+    print_parameters(parameters);
 }
