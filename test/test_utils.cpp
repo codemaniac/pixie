afbfbcf3a55439ba7d8b9f8ac52810af432849f1
Ashish Prasad <ashish.ap.rao@gmail.com>
2024-12-30 19:40:14 +0530

    👌 IMPROVE: Utils Tests

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -1,9 +1,25 @@
 #include "doctest/doctest.h"
+#include "types.h"
 #include "utils.h"
 
 using namespace tejas;
 
 TEST_SUITE("Utils") {
+
+    TEST_CASE("utils::BitScanForward") {
+        u64 x = (1 << 4) | (1 << 8) | (1 << 16);
+
+        u8 pos = utils::BitScanForward(&x);
+        CHECK(pos == 4);
+
+        pos = utils::BitScanForward(&x);
+        CHECK(pos == 8);
+
+        pos = utils::BitScanForward(&x);
+        CHECK(pos == 16);
+
+        CHECK(x == 0ULL);
+    }
 
     TEST_CASE("utils::BitCount1s") {
         CHECK(utils::BitCount1s(0) == 0);
