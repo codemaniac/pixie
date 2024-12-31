955e52d7c5c5eec7830e538553865da820601f4e
Ashish Prasad <ashish.ap.rao@gmail.com>
2024-12-31 09:35:01 +0530

    👌 IMPROVE: Test for movegen

--- test/test_movegen.cpp
+++ test/test_movegen.cpp
@@ -1,7 +1,9 @@
 #include "board.h"
 #include "doctest/doctest.h"
+#include "fen.h"
 #include "movegen.h"
 #include "types.h"
+#include <string>
 
 using namespace tejas;
 
@@ -9,8 +11,11 @@ TEST_SUITE("Movegen") {
 
     TEST_CASE("isSquareAttacked") {
         board::Board board;
-        board.setStartpos();
 
-        REQUIRE(movegen::isSquareAttacked(board, Square::E3, Color::WHITE));
+        std::string fen = "4k3/8/8/4p3/8/8/8/4K3 w - - 0 1";
+        fen::parseFEN(&board, fen);
+
+        REQUIRE(movegen::isSquareAttacked(board, Square::D4, Color::BLACK));
+        REQUIRE(movegen::isSquareAttacked(board, Square::F4, Color::BLACK));
     }
 }
